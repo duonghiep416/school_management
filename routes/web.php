88a0912d9d5c7d67ec1d0250bc3af41201e79247
vanh0101 <132863193+vanh0101@users.noreply.github.com>
2023-05-24 17:04:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
