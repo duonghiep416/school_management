0204dfb9467c5e78132c2e47462a403025dc428f
vanh0101 <132863193+vanh0101@users.noreply.github.com>
2023-05-24 16:58:43 +0700

    Role Wise Menu with Dashboard

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -152,13 +152,15 @@
           <img src="{{ url('public/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{Auth::user()->name}}</a>
         </div>
       </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+      @if(Auth::user()->user_type == 1)
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
             <a href="{{ url('admin/dashboard') }}" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -175,7 +177,54 @@
               </p>
             </a>
           </li>
-          
+
+      @elseif(Auth::user()->user_type == 2)
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="{{ url('teacher/dashboard') }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+        
+
+      @elseif(Auth::user()->user_type == 3)
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="{{ url('student/dashboard') }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+      @elseif(Auth::user()->user_type == 4)
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="{{ url('parent/dashboard') }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+      @endif
+
+
+        
+
+
+          <li class="nav-item">
+            <a href="{{ url('logout')}}" class="nav-link">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+                logout
+              </p>
+            </a>
+          </li>
+         
           
         </ul>
       </nav>
