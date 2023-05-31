1920c81236c7d723793ce751974155023336eacc
vanh0101 <132863193+vanh0101@users.noreply.github.com>
2023-05-31 18:16:41 +0700

    Assign Subject to Class Part - 2

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,12 @@ Route::post('admin/assign_subject/add',[ClassSubjectController::class,'insert'])
 Route::get('admin/assign_subject/edit/{id}',[ClassSubjectController::class,'edit']);
 Route::post('admin/assign_subject/edit/{id}',[ClassSubjectController::class,'update']);
 Route::get('admin/assign_subject/delete/{id}',[ClassSubjectController::class,'delete']);
+Route::get('admin/assign_subject/edit_single/{id}',[ClassSubjectController::class,'edit_single']);
+Route::post('admin/assign_subject/edit_single/{id}',[ClassSubjectController::class,'update_single']);
+
+
+
+
 
 
 
