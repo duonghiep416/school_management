03842966133171e6f8f2de438be932c68d1e19b4
anhdung19 <129310480+anhdung19@users.noreply.github.com>
2023-06-06 04:52:29 +0700

    Form Filters in Student List using Laravel 10

--- resources/views/admin/student/list.blade.php
+++ resources/views/admin/student/list.blade.php
@@ -34,7 +34,101 @@
           <!-- /.col -->
           <div class="col-md-12">
 
-                
+          <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Search Student</h3>
+                </div>
+                    <form method="get" action="">
+                        <div class="card-body">
+                            <div class="row">
+
+
+                            <div class="form-group col-md-2">
+                                <label>Name</label>
+                                <input type ="text" class="form-control" name="name" value="{{ Request::get('name') }}"  placeholder="Name">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Last Name</label>
+                                <input type ="text" class="form-control" name="last_name" value="{{ Request::get('last_name') }}"  placeholder="Last Name">
+                            </div>
+
+                            <div class="form-group col-md-2">
+                                <label>Email</label>
+                                <input type ="text" class="form-control" name="email" value="{{ Request::get('email') }}"  placeholder="Email">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Admission Number</label>
+                                <input type ="text" class="form-control" name="admission_number" value="{{ Request::get('admission_number') }}"  placeholder="Admission Number">
+                            </div>
+
+                            <div class="form-group col-md-2">
+                                <label>Roll Number</label>
+                                <input type ="text" class="form-control" name="roll_number" value="{{ Request::get('roll_number') }}"  placeholder="Roll Number">
+                            </div>
+
+                            <div class="form-group col-md-2">
+                                <label>Class</label>
+                                <input type ="text" class="form-control" name="class_name" value="{{ Request::get('class_name') }}"  placeholder="Class">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Gender</label>
+                                <select class="form-control" name="gender">
+                                        <option value="">Select Gender</option>
+                                        <option {{ (Request::get('gender') == 'Male') ? 'selected' : '' }} value="Male">Male</option>
+                                        <option {{ (Request::get('gender') == 'Female') ? 'selected' : '' }} value="Female">Female</option>
+                                        <option {{ (Request::get('gender') == 'Other') ? 'selected' : '' }} value="Other">Other</option>
+                                </select> 
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Caste</label>
+                                <input type ="text" class="form-control" name="caste" value="{{ Request::get('caste') }}"  placeholder="Caste">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Religion</label>
+                                <input type ="text" class="form-control" name="religion" value="{{ Request::get('religion') }}"  placeholder="Religion">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Mobile Number</label>
+                                <input type ="text" class="form-control" name="mobile_number" value="{{ Request::get('mobile_number') }}"  placeholder="Mobile Number">
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label>Blood Group</label>
+                                <input type ="text" class="form-control" name="blood_group" value="{{ Request::get('blood_group') }}"  placeholder="Blood Group">
+                            </div>
+
+
+                            <div class="form-group col-md-2">
+                                <label>Status</label>
+                                <select class="form-control" name="status">
+                                        <option value="">Select Gender</option>
+                                        <option {{ (Request::get('status') == 100) ? 'selected' : '' }} value="100">Active</option>
+                                        <option {{ (Request::get('status') == 1) ? 'selected' : '' }} value="1">Inactive</option>
+                                        
+                                </select> 
+                            </div>
+                            
+
+                            <div class="form-group col-md-2">
+                                <label>Admission Date</label>
+                                <input type ="date" class="form-control" name="admission_date" value="{{ Request::get('admission_date') }}" >
+                            </div>
+
+                            <div class="form-group col-md-2">
+                                <label>Created Date</label>
+                                <input type ="date" class="form-control" name="date" value="{{ Request::get('date') }}">
+                            </div>
+                            <div class="form-group col-md-3">
+                              <button class="btn btn-primary" type="submit" style="margin-top:30px">Search</button>
+                              <a href="{{ url('admin/student/list') }}" class="btn btn-success" type="submit" style="margin-top:30px">Reset</a>
+                              
+                            </div>
+                            </div>
+                        </div>
+                      
+                    </form>
+                   
+
+                </div>   
 
           
       @include('_message')
@@ -81,12 +175,12 @@
                       <img src="{{ $value -> getProfile() }}" style="height: 50px; width:50px; border-radius: 50px;">
                       @endif
                     </td>
-                    <td>{{ $value -> name }} {{ $value -> last_name }}</td>
-                    <td>{{ $value -> email }}</td>
-                    <td>{{ $value -> admission_number }}</td>
-                    <td>{{ $value -> roll_number }}</td>
-                    <td>{{ $value -> class_name }}</td>
-                    <td>{{ $value -> gender }}</td>
+                    <td>{{ $value ->name }} {{ $value -> last_name }}</td>
+                    <td>{{ $value ->email }}</td>
+                    <td>{{ $value ->admission_number }}</td>
+                    <td>{{ $value ->roll_number }}</td>
+                    <td>{{ $value ->class_name }}</td>
+                    <td>{{ $value ->gender }}</td>
                     <td>
                       @if(!empty($value->date_of_birth))
                       {{ date('d-m-Y', strtotime($value->date_of_birth)) }}
